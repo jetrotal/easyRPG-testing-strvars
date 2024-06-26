aee1346c3415174c11a48765de12ecfeb4b7dcd3
Mauro Junior <45118493+jetrotal@users.noreply.github.com>
2024-06-25 23:01:28 -0300

    rework text that appears

--- irc_handler.js
+++ irc_handler.js
@@ -9,6 +9,40 @@ let logIRC = false;
 
 let nickname = 'ez_' + (Date.now() + Math.floor(Math.random() * 1000)).toString(16);
 
+let user = "";
+let msg = "";
+let lastUser = '';
+let repeatUserCounter = 0;
+let mergedMessage = '';
+let userLastMessageTime = {};
+const COOLDOWN_TIME = 5000; 
+
+function splitStringByLength(str, length = 50) {
+    const segments = str.split('\n');
+    const result = segments.map(segment => {
+      const words = segment.split(' ');
+      let lines = [];
+      let currentLine = '';
+  
+      words.forEach(word => {
+        if ((currentLine + word).length <= length) {
+          currentLine += (currentLine ? ' ' : '') + word;
+        } else {
+          lines.push(currentLine);
+          currentLine = word;
+        }
+      });
+  
+      if (currentLine) {
+        lines.push(currentLine);
+      }
+  
+      return lines.join('\n');
+    });
+  
+    return result.join('\n');
+  }
+
 async function connect() {
   socket = new WebSocket(serverSocket);
 
@@ -22,8 +56,9 @@ function onOpen() {
   console.clear();
   console.log('Connection opened.');
   socket.send(`NICK ${nickname}`);
-  socket.send('USER 0 0 0 :'+nickname+' Bot');
+  socket.send('USER 0 0 0 :'+nickname+' Bot');  
   socket.send('JOIN #EasyRPG_test');
+  socket.send('JOIN #easyrpg');
 }
 
 async function onMessage(event) {
@@ -48,11 +83,53 @@ for (let id of msgTypes)
    console.log('Received:', line);
     if(line.includes("JOIN")) await FileOperations.updateFile(" \\C[4] CONNECTED", "Text/test.txt");   
 
-    if(line.includes("PRIVMSG")){
-let user = " \\C[4]< " + line.split(":")[1].split("[")[0] + " >";
-let msg = " \\C[0] " + line.split("PRIVMSG")[1].split(":")[1];
-    
-await FileOperations.updateFile(user + "\n"+ msg, "Text/test.txt");   
+    // vars were declared outside, on the root of the code
+    if (line.includes("PRIVMSG")) {
+        // Extract the username
+        const currentUser = line.split("!")[0].split(":")[1];
+        user = "\\C[4]< " + currentUser + " >";
+        
+        // Find the position of "PRIVMSG" and the next colon after it
+        let privmsgIndex = line.indexOf("PRIVMSG");
+        let messageStartIndex = line.indexOf(":", privmsgIndex);
+        
+        // Extract the message, keeping all subsequent colons
+        msg = "\\C[0]" + line.slice(messageStartIndex + 1);
+        
+        const currentTime = Date.now();
+        
+        // Check if it's a new user or if the cooldown time has passed
+        if (currentUser !== lastUser || (userLastMessageTime[currentUser] && currentTime - userLastMessageTime[currentUser] > COOLDOWN_TIME)) {
+            // If there's a previous merged message, write it
+            if (mergedMessage) {
+                let splitMergedMessage = splitStringByLength(mergedMessage);
+                await FileOperations.updateFile("\\C[4]< " + lastUser + " >\n" + splitMergedMessage, "Text/test.txt");
+            }
+            
+            // Reset for the new user or after cooldown
+            lastUser = currentUser;
+            repeatUserCounter = 1;
+            mergedMessage = msg;
+        } else {
+            // Same user within cooldown, increment counter and append message
+            repeatUserCounter++;
+            mergedMessage += "\n" + msg;
+            
+            // If we've reached 11 messages, reset the counter and merged message
+            if (repeatUserCounter === 11) {
+                repeatUserCounter = 0;
+                let splitMergedMessage = splitStringByLength(mergedMessage);
+                await FileOperations.updateFile(user + "\n" + splitMergedMessage, "Text/test.txt");
+                mergedMessage = msg;
+            }
+        }
+        
+        // Update the last message time for the current user
+        userLastMessageTime[currentUser] = currentTime;
+        
+        // Always write the current state of the merged message
+        let splitCurrentMessage = splitStringByLength(mergedMessage);
+        await FileOperations.updateFile(user + "\n" + splitCurrentMessage, "Text/test.txt");
     }
     
   // Add more message handling logic here
