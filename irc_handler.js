193a43bd0c11746719980ada097f70455a6188aa
Mauro Junior <45118493+jetrotal@users.noreply.github.com>
2024-06-27 17:21:51 -0300

    Update irc_handler.js

--- irc_handler.js
+++ irc_handler.js
@@ -59,6 +59,7 @@ function onOpen() {
   socket.send('USER 0 0 0 :'+nickname+' Bot');  
   socket.send('JOIN #EasyRPG_test');
   socket.send('JOIN #easyrpg');
+  socket.send('JOIN #make_her_walk');
 }
 
 async function onMessage(event) {
